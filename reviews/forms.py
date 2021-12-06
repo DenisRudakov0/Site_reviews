c8535bf6e9acd64cba9b99f588217a0445efcb14
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-12-06 10:52:33 +0300

    add control update author

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,12 +5,13 @@ class ReviewForm(ModelForm):
     class Meta:
         model = Review
         fields = ['categoru', 'rait', 'review_title', 'prev_text', 'review_text',
-                'image', 'pub_date']
+                'image', 'pub_date', 'author_name']
         widgets = {
             'review_title': TextInput(attrs = {'class': "form-control"}),
             'prev_text': Textarea(attrs = {'class': "form-control"}),
             'review_text': Textarea(attrs = {'class': "form-control"}),
             'pub_date': DateTimeInput(attrs = {'class': "form-control"}),
+            'author_name': TextInput(attrs = {'class': "form-control"}),
         }
 
 class ReviewImageForm(ModelForm):
