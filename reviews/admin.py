70304a92eb945c9341f2c63e1abd557dc89f285d
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-12-06 13:19:49 +0300

    modif admin.py filter search

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,11 +5,24 @@ from .models import Review, Comment, Raiting, ReviewImage, Categoru
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('review_title', 'slug', 'author_name', 'pub_date')
     prepopulated_fields = {'slug': ('review_title', 'author_name')}
+    search_fields = ('review_title', 'review_text')
+    raw_id_fields = ('author_name',)
 
 @admin.register(Categoru)
 class CategoruAdmin(admin.ModelAdmin):
     list_display = ('name_categoru',)
 
-admin.site.register(ReviewImage)
-admin.site.register(Comment)
-admin.site.register(Raiting)
+@admin.register(ReviewImage)
+class ReviewImageAdmin(admin.ModelAdmin):
+    list_display = ('image', 'image_push')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author_name', 'comment_text')
+    search_fields = ('author_name',)
+
+@admin.register(Raiting)
+class RaitingtAdmin(admin.ModelAdmin):
+    list_display = ('review_raiting', 'user_raiting', 'like', 'star')
+    list_filter = ('like', 'star')
+    
