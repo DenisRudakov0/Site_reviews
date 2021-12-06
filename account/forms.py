730ea1b9c7139e1ec73849f843c015b089364019
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-12-06 11:55:41 +0300

    modif widjet forms and review login register welcome html

--- account/forms.py
+++ account/forms.py
@@ -1,20 +1,26 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import ModelForm, TextInput
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(widget = forms.PasswordInput)
+    password = forms.CharField(widget = forms.PasswordInput(attrs = {'class': "form-control"}))
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs = {'class': "form-control"}))
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(attrs = {'class': "form-control"}))
     
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
+        widgets = {
+            'username': TextInput(attrs = {'class': "form-control"}),
+            'first_name': TextInput(attrs = {'class': "form-control"}),
+            'email': TextInput(attrs = {'class': "form-control"}),
+        }
     
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
+            raise forms.ValidationError('Пароли не совпадают')
         return cd['password2']
