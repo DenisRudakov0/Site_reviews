cc93b7288401516bd88d33ca8e569f5e82906d07
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-12-06 20:28:13 +0300

    upgrade cors

--- Site_reviews/settings.py
+++ Site_reviews/settings.py
@@ -187,11 +187,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:8000',
-    'https://glacial-dusk-64788.herokuapp.com/'
+    'https://glacial-dusk-64788.herokuapp.com/',
 ]
 CORS_ORIGIN_REGEX_WHITELIST = [
-    'http://127.0.0.1:8000',
-    'https://glacial-dusk-64788.herokuapp.com/'
+    'https://glacial-dusk-64788.herokuapp.com/',
 ]
 
